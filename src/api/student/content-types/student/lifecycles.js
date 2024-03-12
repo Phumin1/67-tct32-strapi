aecd8c75f8f4f7a93a09f86a3f5e4ee656f201ab
phumin <s6502041510115@email.kmutnb.ac.th>
2024-03-12 15:13:55 +0700

    create lofecycle

--- src/api/student/content-types/student/lifecycles.js
+++ src/api/student/content-types/student/lifecycles.js
@@ -1,9 +1,9 @@
-const md5 =require('md5')
-module.exports = {
-    async beforeCreate(event) {
-        console.log('beforeCreate is worked...',event.param.data);
-        event.param.data.mobile = md5(event.param.data.mobile)
+//const md5 =require('md5')
+//module.exports = {
+   // async beforeCreate(event) {
+     //   console.log('beforeCreate is worked...',event.param.data);
+      //  event.param.data.mobile = md5(event.param.data.mobile)
 
-        return
-    }
-}
+      //  return
+   // }
+//}
